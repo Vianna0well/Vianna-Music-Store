c86ec7acbfbea0887c5cfcac0db9441b2feb590b
Vianna <vianna8523@gmail.com>
2020-05-13 17:58:47 -0300

    Terreno preparado

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css'
+
+import Logo from '../../assets/logo.png'
+
+export default function Repositories() {
+  return (
+    <div className='Container'>
+      <img src={Logo} alt='Vianna Music Store' />
+    </div>
+  )
+}
